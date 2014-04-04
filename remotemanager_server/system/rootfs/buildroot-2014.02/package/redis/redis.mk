bb743743da63226c895315f9ef92eff269926403
damien-dando <damien.d.09@gmail.com>
2014-04-04 22:29:31 +0000

    add redis to systemd

--- remotemanager_server/system/rootfs/buildroot-2014.02/package/redis/redis.mk
+++ remotemanager_server/system/rootfs/buildroot-2014.02/package/redis/redis.mk
@@ -22,6 +22,25 @@ endef
 define REDIS_INSTALL_TARGET_CMDS
         $(TARGET_MAKE_ENV) $(MAKE) $(REDIS_BUILDOPTS) -C $(@D) \
 	    LDCONFIG=true install
+
+	[ -f $(TARGET_DIR)/etc/redis.conf.default ] || \
+		$(INSTALL) -D -m 644 $(@D)/redis.conf \
+			$(TARGET_DIR)/etc/redis.conf.default
+
+	[ -f $(TARGET_DIR)/etc/redis.conf ] || \
+		ln -fs redis.conf.default \
+			$(TARGET_DIR)/etc/redis.conf
+endef
+
+define REDIS_INSTALL_INIT_SYSTEMD
+	[ -f $(TARGET_DIR)/etc/systemd/system/redis.service ] || \
+		$(INSTALL) -D -m 644 package/redis/redis.service \
+			$(TARGET_DIR)/etc/systemd/system/redis.service
+
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+	ln -fs ../redis.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/redis.service
 endef
 
 $(eval $(generic-package))
