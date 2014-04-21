011e746b95460483fe64b768c33a0e65f68d8b4e
damien-dando <damien.d.09@gmail.com>
2014-04-21 16:15:41 +0000

    use latest release for arvdude sources

--- remotemanager_server/system/rootfs/buildroot-2014.02/package/avrdude/avrdude.mk
+++ remotemanager_server/system/rootfs/buildroot-2014.02/package/avrdude/avrdude.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-AVRDUDE_VERSION = eabe067c4527bc2eedc5db9288ef5cf1818ec720
-AVRDUDE_SITE = $(call github,kcuzner,avrdude,$(AVRDUDE_VERSION))
+AVRDUDE_VERSION = 6.1
+AVRDUDE_SOURCE = avrdude-$(AVRDUDE_VERSION).tar.gz
+AVRDUDE_SITE = http://download.savannah.gnu.org/releases/avrdude
 AVRDUDE_LICENSE = GPLv2+
-AVRDUDR_LICENSE_FILES = avrdude/COPYING
-AVRDUDE_SUBDIR = avrdude
+AVRDUDR_LICENSE_FILES = COPYING
 # Sources coming from git, without generated configure and Makefile.in
 # files.
-AVRDUDE_AUTORECONF = YES
+#AVRDUDE_AUTORECONF = YES
 AVRDUDE_DEPENDENCIES = libelf libusb libusb-compat ncurses \
 	host-flex host-bison
 
