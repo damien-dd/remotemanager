a76cf407b30d567c5593427ae5dde3e9b88d8546
damien-dando <damien.d.09@gmail.com>
2014-04-23 21:31:54 +0000

    initial recovery interface page

--- remotemanager_server/remotemanager/recovery_interface/recovery_interface.py
+++ remotemanager_server/remotemanager/recovery_interface/recovery_interface.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
