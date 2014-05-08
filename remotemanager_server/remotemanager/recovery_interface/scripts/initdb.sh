55acaeaaa775d3b67c75e1e0549e38d243a1816e
damien-dando <damien.d.09@gmail.com>
2014-05-08 20:25:28 +0000

    add option to reformat the partition to the initdb.sh script

--- remotemanager_server/remotemanager/recovery_interface/scripts/initdb.sh
+++ remotemanager_server/remotemanager/recovery_interface/scripts/initdb.sh
@@ -8,8 +8,41 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-echo 'Delete the all pgsql directory...'
-/bin/rm -rf /data/pgsql
+if [ $# -eq 1 ] && [ $1 == "-reformat" ]; then
+	echo 'Umount /data partition...'
+	! mountpoint -q /data || /bin/umount /data
+	if [ $? -ne 0 ]; then
+		echo 'Error: Cannot umount the data partition'
+		exit 1
+	fi
+
+	/usr/sbin/mkfs.ext4 /dev/mmcblk0p3
+	if [ $? -ne 0 ]; then
+		echo 'Error: Cannot format the data partition'
+		exit 1
+	fi
+
+	/bin/mount /data
+	if [ $? -ne 0 ]; then
+		echo 'Error: Cannot mount the data partition'
+		exit 1
+	fi
+
+	/bin/mkdir /data/dbbackup
+	if [ $? -ne 0 ]; then
+		echo 'Error: Cannot create /data/dbbackup directory'
+		exit 1
+	fi
+
+	/bin/chown www-data:www-data /data/dbbackup
+	if [ $? -ne 0 ]; then
+		echo 'Error: Cannot set ownership on /data/dbbackup directory'
+		exit 1
+	fi
+else
+	echo 'Delete the all pgsql directory...'
+	/bin/rm -rf /data/pgsql
+fi
 
 echo 'Restart PostgreSQL services...'
 /usr/bin/systemctl start postgresql.service
