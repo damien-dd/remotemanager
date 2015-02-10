6628e9a7ab2cdc3075b367f112451e1b55ad76a4
damien-dando <damien.d.09@gmail.com>
2015-02-10 17:03:30 +0000

    add short delay before showing the tooltip on timeline charts

---
+++ remotemanager_server/remotemanager/main_app/static/js/tooltip-delay.js
@@ -0,0 +1,33 @@
+(function (H) {
+
+    var timerId;
+	
+
+    H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
+
+        var point = arguments[1];
+        var chart = this.chart;
+        var tooltip = this;
+        var refreshArguments = arguments;
+        var delayForDisplay = chart.options.tooltip.delayForDisplay;
+
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+
+        if (delayForDisplay) {
+	
+            timerId = window.setTimeout(function () {
+
+                if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point) > -1) {
+                    proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+                }
+
+            }, delayForDisplay || 1000);
+        } else {
+            proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+        }
+
+    });
+
+}(Highcharts));
